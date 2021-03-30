32c380a6e1e27b71b027894dd58351e2b39d7ac0
Tarun Soni <soni.tarun26@gmail.com>
2021-03-30 18:58:42 +0530

    basic backend setup

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+const app = express()
+
+app.use(express.json())
+dotenv.config()
+
+app.use(cors())
+
+app.get('/api/config/bookKey', (req, res) => {
+  res.send(process.env.REACT_APP_GBOOK_API_KEY)
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(
+  PORT,
+  console.log(`server running in ${process.env.NODE_ENV} env on  port ${PORT}`)
+)
+if (process.env.NODE_ENV === 'production') {
+  //set frontend/build as static folder
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API running....')
+  })
+}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 
+import { Button } from 'react-bootstrap'
+import axios from 'axios'
 const App = () => {
-  return (
-    <div className="App">
-      <h1>hello</h1>
-    </div>
-  )
+  const [bookInput, setBookInput] = useState('harrypotter')
+  // const [apiKey] = useState(process.env.REACT_APP_GBOOK_API_KEY)
+  useEffect(() => {
+    async function getData() {
+      const { data: apiKey } = await axios.get('/api/config/bookKey')
+      const url = `https://www.googleapis.com/books/v1/volumes?q=${bookInput}&filter=free-ebooks&key=${apiKey}`
+      const resp = await axios.get(url)
+
+      console.log(`resp.data`, resp.data)
+    }
+    getData()
+  }, [bookInput])
+
+  return <div className="App"></div>
 }
 
 export default App
