52e520a50a580e2385c392cd019a0e6d462fcf35
Tarun Soni <soni.tarun26@gmail.com>
2021-04-01 18:57:19 +0530

    bookModel

---
+++ backend/models/bookModel.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose'
+
+const bookSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    imageUrl: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    timestamps: true
+  }
+)
+
+const Book = mongoose.model('Book', bookSchema)
+
+export default Book
